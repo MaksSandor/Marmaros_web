6ca41c162592607966be3d9ee9afe4b7d37cbbb9
MaksSandor <sandormaks@gmail.com>
2025-07-02 21:59:46 +0300

    Добавив базовий стиль на тур-карточкі

--- FrontEnd/client/src/ToursPage/ToursPage.js
+++ FrontEnd/client/src/ToursPage/ToursPage.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Header from "../header";
+import useTourCards from "./useTourCards"; 
+import style_cards from "./cards_style.module.css";
+import style_page from "./page_style.module.css";
 
 function TourPage() {
-    
-    return (
-        <div>
-            <Header />
-            <h1>Tour Page</h1>
-        </div>
-    )
+  const tours = useTourCards();
+
+  return (
+    <div className={style_page.page}>
+      <Header />
+      <div className={style_cards.cards_place}>
+          {tours.map((tour) => (
+                <div key={tour.id} className={style_cards.card}>
+              <img src={tour.img} alt={tour.name} className={style_cards.img} />
+              <h1 className={style_cards.title}>{tour.name}</h1>
+              <div className={style_cards.places}>
+                <p className={style_cards.free}>Вільних місць: <span>{tour.freePlaces}</span></p>
+                <p className={style_cards.max}>Макс. місць: <span>{tour.maxPlaces}</span></p>
+              </div>  
+              <p className={style_cards.price}>Ціна: {tour.price} грн</p>
+                </div>))}
+      </div>
+    </div>
+  );
 }
 
 export default TourPage;
---
+++ FrontEnd/client/src/ToursPage/useTourCards.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function useTourCards() {
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/tours') 
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Помилка при завантаженні турів');
+        }
+        return response.json();
+      })
+      .then(data => setTours(data))
+      .catch(error => console.error('Помилка при fetch:', error));
+  }, []);
+
+  return tours;
+}
+
+export default useTourCards;
