067487a36e89f85024c79f9f03d59f8d3afb46b4
MaksSandor <sandormaks@gmail.com>
2025-07-03 12:46:36 +0300

    заабгрейдив тур-карточкі

--- FrontEnd/client/src/ToursPage/ToursPage.js
+++ FrontEnd/client/src/ToursPage/ToursPage.js
@@ -13,13 +13,17 @@ function TourPage() {
       <div className={style_cards.cards_place}>
           {tours.map((tour) => (
                 <div key={tour.id} className={style_cards.card}>
-              <img src={tour.img} alt={tour.name} className={style_cards.img} />
-              <h1 className={style_cards.title}>{tour.name}</h1>
-              <div className={style_cards.places}>
-                <p className={style_cards.free}>Вільних місць: <span>{tour.freePlaces}</span></p>
-                <p className={style_cards.max}>Макс. місць: <span>{tour.maxPlaces}</span></p>
-              </div>  
-              <p className={style_cards.price}>Ціна: {tour.price} грн</p>
+                    <video autoPlay muted loop playsInline className={style_cards.backgroundVideo}>
+                        <source src={tour.mp4} type="video/mp4" />
+                        Відео не завантажене.
+                    </video>
+                    <img src={tour.img} alt={tour.name} className={style_cards.img} />
+                    <h1 className={style_cards.title}>{tour.name}</h1>
+                    <div className={style_cards.places}>
+                        <p className={style_cards.free}>Вільних місць: <span>{tour.freePlaces}</span></p>
+                        <p className={style_cards.max}>Макс. місць: <span>{tour.maxPlaces}</span></p>
+                    </div>  
+                    <p className={style_cards.price}>Ціна: {tour.price} грн</p>
                 </div>))}
       </div>
     </div>
