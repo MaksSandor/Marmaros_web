c70af27ad2c2dfe49a88338019db63534a795597
MaksSandor <sandormaks@gmail.com>
2025-07-08 12:31:08 +0300

    useNavigate

--- FrontEnd/client/src/ToursPage/ToursPage.js
+++ FrontEnd/client/src/ToursPage/ToursPage.js
@@ -3,9 +3,13 @@ import Header from "../headerImg/header";
 import useTourCards from "./useTourCards"; 
 import style_cards from "./cards_style.module.css";
 import style_page from "./page_style.module.css";
+import { useNavigate } from "react-router-dom";
 
 function TourPage() {
   const tours = useTourCards();
+  const navigate = useNavigate();
+
+
 
   return (
     <div className={style_page.page}>
@@ -15,12 +19,12 @@ function TourPage() {
       </div>
       <div className={style_cards.cards_place}>
           {tours.map((tour) => (
-                <div key={tour.id} className={style_cards.card}>
-                    <video autoPlay muted loop playsInline className={style_cards.backgroundVideo}>
+                <div key={tour.id} className={style_cards.card} onClick={() => {navigate(`/tours/${tour.name}`)}}>
+                    <video autoPlay muted loop playsInline className={style_cards.backgroundVideo} >
                       <source src={`http://localhost:3001${tour.mp4}`} type="video/mp4" />
                       Відео не завантажене.
                     </video>
-                    <img src={tour.img} alt={tour.name} className={style_cards.img}/>
+                    <img src={tour.img} alt={tour.name} className={style_cards.img} />
                     <h1 className={style_cards.title}>{tour.name}</h1>
                     <div className={style_cards.places}>
                         <p className={style_cards.free}>Вільних місць: <span>{tour.freePlaces}</span></p>
